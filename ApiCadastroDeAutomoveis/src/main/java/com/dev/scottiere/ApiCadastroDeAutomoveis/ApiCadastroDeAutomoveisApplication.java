691d312b8285b20ca44fa535176acc207ff5c8a9
gustavoscottiere <gustavo.scottiere@hotmail.com>
2026-09-11 11:05:12 -0300

    Inicio do Projeto de api de carros populares

---
+++ ApiCadastroDeAutomoveis/src/main/java/com/dev/scottiere/ApiCadastroDeAutomoveis/ApiCadastroDeAutomoveisApplication.java
@@ -0,0 +1,13 @@
+package com.dev.scottiere.ApiCadastroDeAutomoveis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCadastroDeAutomoveisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCadastroDeAutomoveisApplication.class, args);
+	}
+
+}
---
+++ ApiCadastroDeAutomoveis/src/test/java/com/dev/scottiere/ApiCadastroDeAutomoveis/ApiCadastroDeAutomoveisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.scottiere.ApiCadastroDeAutomoveis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCadastroDeAutomoveisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
